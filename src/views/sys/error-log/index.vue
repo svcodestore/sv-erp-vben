bdfd533c4ca4c47a949a5722fefab493085f9ad8
yangwb1123 <1832227346@qq.com>
2022-04-29 16:03:12 +0800

    fix: error-log page

--- src/views/sys/error-log/index.vue
+++ src/views/sys/error-log/index.vue
@@ -36,7 +36,6 @@
   import { useMessage } from '/@/hooks/web/useMessage';
   import { useI18n } from '/@/hooks/web/useI18n';
   import { useErrorLogStore } from '/@/store/modules/errorLog';
-  import { fireErrorApi } from '/@/api/demo/error';
   import { getColumns } from './data';
   import { cloneDeep } from 'lodash-es';
 
@@ -86,7 +85,5 @@
     imgList.value.push(`${new Date().getTime()}.png`);
   }
 
-  async function fireAjaxError() {
-    await fireErrorApi();
-  }
+  async function fireAjaxError() {}
 </script>
