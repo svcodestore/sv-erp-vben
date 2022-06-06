bf2fc8987d7bd042366a62b427f68b345cac4092
yangwb1123 <1832227346@qq.com>
2022-06-06 11:00:47 +0800

    fix: 自动排程月看板行事历UI

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -9,6 +9,16 @@ const DATE_FORMAT = 'YYYY-MM-DD';
 export type DateUtil = Dayjs;
 export const dateUtil = dayjs;
 
+export const weekdayMap = {
+  0: '日',
+  1: '一',
+  2: '二',
+  3: '三',
+  4: '四',
+  5: '五',
+  6: '六',
+};
+
 export function formatToDateTime(
   date: DateUtil | undefined = undefined,
   format = DATE_TIME_FORMAT,
--- src/views/PROD/Schedule/View/Month/Report/Calendar.vue
+++ src/views/PROD/Schedule/View/Month/Report/Calendar.vue
@@ -1,15 +1,15 @@
 <template>
   <table width="100%" style="border-collapse: collapse">
-    <tr v-for="(_, idx) in Array(maxWeeksInMonth)" :key="idx">
+    <tr v-for="(_, idx) in Array(daysArr.length / 7)" :key="idx">
       <td
         style="border: 1px solid black; padding: 0; margin: 0"
         v-for="(__, i) in Array(7)"
         :key="i"
       >
-        <div style="display: flex; height: 120px" v-if="!isCurrentMonthDay(idx)">
+        <div style="display: flex; height: 120px">
           <span style="font-size: 14px"> {{ getWeekday(daysArr[0]) }} </span>
           <span style="margin: auto; font-size: 14px">
-            <slot name="cellContent" :date="getDate(daysArr[0])"></slot>
+            <slot name="cellContent" :date="daysArr[0] && getDate(daysArr[0])"></slot>
           </span>
           <span style="font-size: 14px; margin-left: auto">
             {{ daysArr.shift() }}
@@ -23,7 +23,7 @@
 <script lang="ts" setup>
   import moment from 'moment';
   import { computed } from 'vue';
-  import { DateUtil, dateUtil } from '/@/utils/dateUtil';
+  import { DateUtil, dateUtil, weekdayMap } from '/@/utils/dateUtil';
   import { range } from '/@/utils/helper';
 
   const props = defineProps({
@@ -34,35 +34,27 @@
   });
 
   const dateObj = computed(() => dateUtil(props.date));
-  const daysArr = computed(() => range(1, dateObj.value.daysInMonth() + 1));
-  const weekdayMap = {
-    0: '日',
-    1: '一',
-    2: '二',
-    3: '三',
-    4: '四',
-    5: '五',
-    6: '六',
-  };
+  const daysInMonth = computed(() => dateObj.value.daysInMonth());
+
   const getDate = (day: number) =>
-    moment(dateObj.value.format('YYYY/MM/' + +(day > 9 ? day : '0' + day)));
-  const getWeekday = (day: number) => weekdayMap[getDate(day).days()];
-  const firstWeekday = computed(() => getDate(daysArr.value[0]).days());
-  const lastWeekday = computed(() => getDate(daysArr.value[daysArr.value.length - 1]).days());
+    moment(dateObj.value.format('YYYY/MM/') + (day > 9 ? day : '0' + day));
+  const getWeekday = (day?: number) => day && weekdayMap[getDate(day).days()];
+  const firstWeekday = computed(() => getDate(1).days());
+  const lastWeekday = computed(() => getDate(daysInMonth.value).days());
 
   const headFill = computed(() => (firstWeekday.value === 0 ? 6 : firstWeekday.value - 1));
-  const tailFill = computed(() => 6 - lastWeekday.value);
-  const maxWeeksInMonth = 5;
-  daysArr.value.unshift(...Array(headFill.value));
-  daysArr.value.push(...Array(tailFill.value));
+  const tailFill = computed(() => 7 - lastWeekday.value);
+  const starAt = computed(() => 7 - headFill.value);
+  const endAt = computed(() => daysInMonth.value - lastWeekday.value);
+  const headWeekArr = computed(() => [...Array(headFill.value), ...range(1, starAt.value + 1)]);
+  const tailWeekArr = computed(() => [
+    ...range(endAt.value + 1, daysInMonth.value + 1),
+    ...Array(tailFill.value),
+  ]);
 
-  const unavailableDaysLen = computed(() => daysArr.value.filter((d) => d === undefined).length);
-
-  const isCurrentMonthDay = (idx: number) => {
-    return (
-      daysArr.value.every((d) => d === undefined) &&
-      (unavailableDaysLen.value % 7 === 0 ||
-        (unavailableDaysLen.value % 7 !== 0 && idx === maxWeeksInMonth - 1))
-    );
-  };
+  const daysArr = computed(() => [
+    ...headWeekArr.value,
+    ...range(starAt.value + 1, endAt.value + 1),
+    ...tailWeekArr.value,
+  ]);
 </script>
