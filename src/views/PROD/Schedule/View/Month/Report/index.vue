29bc1d3192b76499f4f94372c1691e34f5ab3939
yangwb1123 <1832227346@qq.com>
2022-06-06 15:17:25 +0800

    refactor(ui): 自动排程报表

--- src/views/PROD/Schedule/View/Month/Report/index.vue
+++ src/views/PROD/Schedule/View/Month/Report/index.vue
@@ -8,30 +8,82 @@
       </template>
     </Calendar>
   </div>
-  <table
-    v-for="(tb, i) in tableData"
-    :key="i"
-    :style="`text-align: center; width: 100%;border-bottom: 1px solid transparent;`"
-  >
-    <tr>
-      <td style="width: 30px">#{{ i + 1 }}</td>
-      <td
-        v-for="(v, idx) in tb.phases"
-        :key="idx"
-        v-html="v"
-        :style="idx < 6 ? `width: 72px;` : idx < 10 ? 'width: 60px;' : ''"
-      ></td>
-    </tr>
+  <table style="text-align: center; width: 100%; border-collapse: collapse">
+    <template v-for="record in data" :key="record">
+      <tr>
+        <td style="border: 1px solid black">客户代号</td>
+        <td style="border: 1px solid black">客户订单号</td>
+        <td style="border: 1px solid black">工厂品号</td>
+        <td style="border: 1px solid black">订单数量</td>
+        <td rowspan="3" style="border: 1px solid black">副流程</td>
+        <td
+          v-for="phase in record.phases.filter((e) => !e.isMaster)"
+          :key="phase"
+          style="border: 1px solid black"
+        >
+          {{ phase.name }}
+        </td>
+      </tr>
+      <tr>
+        <td rowspan="5" style="border: 1px solid black">{{ record.itemCode }}</td>
+        <td rowspan="5" style="border: 1px solid black">{{ record.customerNo }}</td>
+        <td rowspan="5" style="border: 1px solid black">{{ record.customerPoNo }}</td>
+        <td rowspan="5" style="border: 1px solid black">{{ record.itemQty }}</td>
+        <td
+          v-for="phase in record.phases.filter((e) => !e.isMaster)"
+          :key="phase"
+          style="border: 1px solid black"
+        >
+          {{ phase.startAt.substring(5).substring(0, 11) }}
+        </td>
+      </tr>
+      <tr>
+        <td
+          v-for="phase in record.phases.filter((e) => !e.isMaster)"
+          :key="phase"
+          style="border: 1px solid black"
+        >
+          {{ phase.completeAt.substring(5).substring(0, 11) }}
+        </td>
+      </tr>
+      <tr>
+        <td rowspan="3" style="border: 1px solid black">主流程</td>
+        <td
+          v-for="phase in record.phases.filter((e) => !!e.isMaster)"
+          :key="phase"
+          style="border: 1px solid black"
+        >
+          {{ phase.name }}
+        </td>
+      </tr>
+      <tr>
+        <td
+          v-for="phase in record.phases.filter((e) => !!e.isMaster)"
+          :key="phase"
+          style="border: 1px solid black"
+        >
+          {{ phase.completeAt.substring(5).substring(0, 11) }}
+        </td>
+      </tr>
+      <tr>
+        <td
+          v-for="phase in record.phases.filter((e) => !!e.isMaster)"
+          :key="phase"
+          style="border: 1px solid black"
+        >
+          {{ phase.completeAt.substring(5).substring(0, 11) }}
+        </td>
+      </tr>
+    </template>
   </table>
 </template>
 
 <script lang="ts" setup>
   import Header from './Header.vue';
   import Calendar from './Calendar.vue';
-  import { computed, unref } from 'vue';
   import { DateUtil } from '/@/utils/dateUtil';
 
-  const props = defineProps({
+  defineProps({
     data: {
       type: Array as PropType<any[]>,
       default: () => [],
@@ -40,46 +92,5 @@
       type: Object as PropType<DateUtil>,
       default: () => ({}),
     },
-  });
-
-  const tableData = computed(() => {
-    const arr = [] as any[];
-    unref(props.data).forEach((item) => {
-      const commonFields = ['客户代号', '客户订单号', '工厂品号', '订单数量', '生产单位'];
-      const vals = [
-        item.customerNo,
-        item.customerPoNo,
-        item.itemCode,
-        item.itemQty,
-        item.workshopName,
-      ];
-      const keys = commonFields.map((field) => `<span>${field}</span><br>`);
-      const values = vals.map((value) => `<span>${value}</span><br>`);
-
-      item.phases.forEach((e) => {
-        const { name, isMaster, costTime, startAt, completeAt, outTime, workerNum } = e;
-        keys.push(
-          `<span>${name}</span>/<span style="font-weight: bolder; color: black;">${
-            isMaster === 0 ? '副' : '主'
-          }</span>${
-            outTime > 0
-              ? '/<i style="font-weight: bolder; color: rgba(250, 20, 40, 0.69);">外</i>'
-              : ''
-          }${costTime > 0 ? `<span>/</span><span>${costTime}</span>` : ''}/${workerNum}`,
-        );
-
-        values.push(
-          `<br><span>${startAt.substring(0, 16)}</span>${
-            costTime > 0 || outTime > 0 ? `<br/><span>${completeAt.substring(0, 16)}</span>` : ''
-          }`,
-        );
-      });
-
-      arr.push({
-        phases: keys.map((k, i) => `<div>${k}\n${values[i]}</div>`),
-      });
-    });
-
-    return arr;
   });
 </script>
