5bdec18293a1a26d84ed96f9bbaa227a745e2505
yangwb1123 <1832227346@qq.com>
2022-06-08 10:40:00 +0800

    feat(utils): add `debouce`, `objArrUniq`

--- src/utils/helper/index.ts
+++ src/utils/helper/index.ts
@@ -1,1 +1,33 @@
+import { debounce as dbce } from 'lodash-es';
+
 export const range = (s: number, e: number) => new Array(e - s).fill(s).map((_, i) => s + i);
+
+export const debounce = (fn: Fn, time?: number) => {
+  return dbce(fn, time || 330);
+};
+
+export function objArrUniq(arr: Record<string, any>[], pk?: any) {
+  const o = {};
+  const newArr = arr.reduce((item, next) => {
+    let _pk = Object.keys(next).join(',,') + '-' + Object.values(next).join(',,');
+    if (typeof pk === 'string') {
+      _pk = pk + '-' + next[pk];
+    } else if (Array.isArray(pk)) {
+      _pk =
+        pk.join(',,') +
+        '-' +
+        pk.reduce((prev, curr) => {
+          return prev + next[curr] + ',,';
+        }, '');
+    }
+
+    if (!o[_pk]) {
+      o[_pk] = true;
+      item.push(next);
+    }
+
+    return item;
+  }, []);
+
+  return newArr;
+}
