927930aeac5cad72ca0c427a8b87a6306e024226
yangwb1123 <1832227346@qq.com>
2022-06-10 17:04:30 +0800

    feat(websocket): init websocket

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,9 @@
 
   import 'dayjs/locale/zh-cn';
   import { useAppStore } from '/@/store/modules/app';
+  import { ref, onMounted, onUnmounted } from 'vue';
+  import { useUserStore } from '/@/store/modules/user';
+
   // support Multi-language
   const { getAntdLocale } = useLocale();
 
@@ -23,4 +26,42 @@
   const appStore = useAppStore();
 
   appStore.initCurrentApplication();
+
+  const ws = ref<WebSocket>();
+  const wsTimerId = ref<number>();
+
+  const handleConn = () => {
+    const userStore = useUserStore();
+    const userId = userStore.getUserInfo.id;
+    console.log(userId);
+    if (!userId) return;
+    ws.value = new WebSocket(`ws://localhost:8888/ws/user-activation/` + userId);
+    ws.value.onopen = function () {
+      console.log('OPEN');
+    };
+    ws.value.onclose = function () {
+      console.log('CLOSE');
+      ws.value = undefined;
+    };
+    ws.value.onmessage = function (evt) {
+      console.log('RESPONSE: ' + evt.data);
+    };
+    ws.value.onerror = function () {
+      console.log('ERROR: ');
+    };
+  };
+
+  onMounted(() => {
+    setTimeout(() => {
+      handleConn();
+    }, 1500);
+    wsTimerId.value = setInterval(() => {
+      ws.value?.send('ping');
+    }, 15000) as unknown as number;
+  });
+
+  onUnmounted(() => {
+    ws.value?.close();
+    clearInterval(wsTimerId.value);
+  });
 </script>
